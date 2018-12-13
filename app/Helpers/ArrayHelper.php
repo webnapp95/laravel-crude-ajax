bb53cb3695819b5bfc535901a482b2a272674e88
chirag Bhalara <info@webnappdev.in>
2018-12-13 22:00:43 +0530

    Update Array Helper
    Update Array Helper

--- app/Helpers/ArrayHelper.php
+++ app/Helpers/ArrayHelper.php
@@ -8,7 +8,7 @@
         }
 
         public static function diffRecursive($array1, $array2) {
-            $difference=array();
+            $difference = array();
             foreach($array1 as $key => $value) {
                 if(is_array($value) && isset($array2[$key])){ // it's an array and both have the key
                     $new_diff = self::diffRecursive($value, $array2[$key]);
@@ -20,6 +20,19 @@
                     $difference[$key] = "Missing from the second array";
                 }
             }
+
+            foreach($array2 as $key => $value) {
+                if(is_array($value) && isset($array1[$key])){ // it's an array and both have the key
+                    $new_diff = self::diffRecursive($value, $array1[$key]);
+                    if( !empty($new_diff) )
+                        $difference[$key] = $new_diff;
+                } else if(is_string($value) && !in_array($value, $array1)) { // the value is a string and it's not in array B
+                    $difference[$key] = $value . " is missing from the first array";
+                } else if(!is_numeric($key) && !array_key_exists($key, $array1)) { // the key is not numberic and is missing from array B
+                    $difference[$key] = "Missing from the first array";
+                }
+            }
+
             return $difference;
         }
 
@@ -31,6 +44,10 @@
             if ($encode) {
                 return json_encode($json);
             }
+            if(!is_array($json)) {
+                return $json;
+            }
+
             return json_decode($json, $toAssociate);
         }
 
@@ -44,14 +61,6 @@
 
         public static function StrToArray($inputString, $separator) {
             return explode($separator, $inputString);
-        }
-
-        public static function recursiveSearch(array $array, $search, $mode = 'value', $return = false) {
-            foreach (new RecursiveIteratorIterator(new recursiveArrayIterator($array)) as $key => $value) {
-                if($search === ${${"mode"}}) {
-                    return $return ? ${${"mode"}} : true;
-                }
-            }
         }
 
     }

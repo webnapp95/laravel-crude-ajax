6df48fffa40d4893d7b1ee07f8e017852e070632
chirag Bhalara <info@webnappdev.in>
2019-01-06 22:19:21 +0530

    implement distance from lat and long
    implement distance from lat and long

---
+++ app/Helpers/AppHelper.php
@@ -0,0 +1,33 @@
+<?php
+    namespace App\Helpers;
+
+    class AppHelper {
+
+        public function __construct() {
+        }
+
+// $unit M = Miles, K = Kilometers, N = Nautical Miles
+        public static function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+          if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+            return 0;
+          }
+          else {
+            $theta = $lon1 - $lon2;
+            $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+            $dist = acos($dist);
+            $dist = rad2deg($dist);
+            $miles = $dist * 60 * 1.1515;
+            $unit = strtoupper($unit);
+
+            if ($unit == "K") {
+              return ($miles * 1.609344);
+            } else if ($unit == "N") {
+              return ($miles * 0.8684);
+            } else {
+              return $miles;
+            }
+          }
+        }
+
+    }
+?>
